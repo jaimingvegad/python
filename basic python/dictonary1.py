bc8a53dab68f96a59caf0ced0257138a441d705f
jaimingvegad <jaimingvegad@gmail.com>
2022-10-12 19:33:09 +0530

    project

--- basic python/dictonary1.py
+++ basic python/dictonary1.py
@@ -1,6 +1,6 @@
 #example of dictonary
 
-book = {'name':'the greats','price':89.75,'pages':789,'author':'yesh'}
+book = {'name':'the greats','price':89.75,'pages':789,'author':'yesh' }
 print(book)
 book['author'] = 'yesh'
 print(book['author'])
